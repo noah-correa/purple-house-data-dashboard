63c540290b4792226f2396b554b746014dd24676
Noah Correa <noah.correa28@gmail.com>
2022-10-26 16:02:42 +1100

    updated test file generator script

--- testDataGenerator.py
+++ testDataGenerator.py
@@ -1,14 +1,33 @@
 # Script for generating test data
 import datetime
 import random
+import argparse
 
-FILE_NAME = 'testDataYear'
+period_choices = ['day', 'month', 'year']
+
+parser = argparse.ArgumentParser(prog='testDataGenerator', description='Simulate temperature data')
+parser.add_argument('period', choices=period_choices, help='Time period (day, month or year)')
+
+args = parser.parse_args()
+
+period = args.period
+print(period)
+days = 0
+if period == 'day':
+	days = 0
+elif period == 'month':
+    days = 30
+elif period == 'year':
+    days = 365
+
+
+FILE_NAME = 'testData' + period.capitalize()
 START_DATE = '25 10 2021'		# Format like 'dd mm yyyy'
 
 # today = time(date.today().strftime('%d %b %Y')
 d, m, y = START_DATE.split(' ')
 today = datetime.date.today()
-start = datetime.date(int(y), int(m), int(d))
+start = datetime.date.today() - datetime.timedelta(days=days)
 
 times = ['13:42:26','13:52:33','13:32:18','13:22:11','13:12:04','13:01:57','12:51:50','12:41:43','12:31:36','12:21:28','12:11:21','12:01:14','11:51:07','11:41:00','11:30:52','11:20:45','11:10:38','11:00:31','10:50:23','10:40:16','10:30:09','10:20:02','10:09:55','09:59:48','09:49:40','09:39:33','09:29:26','09:19:19','09:09:12','08:59:05']
 
